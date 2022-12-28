64810af24a2377ceffaffcb89fcf99ad30b867fd
Yusuf Ugurlu <108735272+yugurlu@users.noreply.github.com>
2022-12-28 12:32:47 +0300

    FİNİTOOOOO

---
+++ push_swap/push_swap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yugurlu <yugurlu@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/07 13:53:15 by yugurlu           #+#    #+#             */
+/*   Updated: 2022/12/28 12:31:27 by yugurlu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	t_list	*data;
+
+	data = malloc(sizeof(t_list));
+	if (ac > 1)
+	{
+		data->av = av;
+		if (!check_and_creat(data))
+		{
+			free_all(data);
+			return (1);
+		}
+		if (a_is_sorted(data) && data->alen <= 5)
+			small_sort(data);
+		else if (a_is_sorted(data))
+			radix(data);
+	}
+	free_all(data);
+	return (0);
+}
---
+++ push_swap/utils3.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils3.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yugurlu <yugurlu@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/25 14:33:46 by yugurlu           #+#    #+#             */
+/*   Updated: 2022/12/27 13:56:34 by yugurlu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	free_split(t_list *data)
+{
+	int	i;
+
+	i = 0;
+	while (data->split[i])
+		free(data->split[i++]);
+	free(data->split);
+}
+
+void	back_fill_stack(t_list *data)
+{
+	int	i;
+
+	i = 0;
+	while (data->alen > i)
+	{
+		data->stacka[i] = data->fakesorted[i];
+		i++;
+	}
+}
+
+bool	max_min_int(t_list *data)
+{
+	int	i;
+
+	i = 0;
+	while (data->stacka[i])
+	{
+		if (data->stacka[i] == 2147483647 || data->stacka[i] == -2147483648)
+			return (true);
+		i++;
+	}
+	return (false);
+}
